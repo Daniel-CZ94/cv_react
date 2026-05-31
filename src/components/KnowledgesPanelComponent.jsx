9599768b617854e2973f36e909fd871af1190e71
Daniel-CZ94 <danielzamacona94@outlook.com>
2026-05-30 18:37:41 -0600

    Se realizaron modificaciones en el diseño del CV y se actualizaron los datos por unos datos reales

--- src/components/KnowledgesPanelComponent.jsx
+++ src/components/KnowledgesPanelComponent.jsx
@@ -1,22 +1,92 @@
+import { text } from "@fortawesome/fontawesome-svg-core";
 import { faHandPaper } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { Alert } from "react-bootstrap";
+import { Alert, Col, Container, Row } from "react-bootstrap";
 const KnowledgesPanelComponent = ({data}) => {
-    return(
+    if(data.length > 0 && data.length <= 15){
+        return(
         <>
-        <Alert variant="primary">
+            <Alert variant="primary">
                 <div style={{textAlign: "left"}}>
                     <h5 className="text-info"><FontAwesomeIcon icon={faHandPaper}/> &nbsp;Conocimientos</h5>
                 </div>
             </Alert>
             <div style={{textAlign:"left"}}>
-                    <ul>
-                        {
-                            data.map((item)=><li key={item.idConocimiento}>{item.descripcion} (Nivel {item.nivel})</li>)
-                        }
-                    </ul>
+                <Container>
+                    <Row>
+                        <Col>
+                            <ul>
+                                {
+                                    data.map((item)=><li key={item.idConocimiento}>{item.descripcion} (Nivel {item.nivel})</li>)
+                                }
+                            </ul>
+                        </Col>
+                    </Row>
+                </Container>
+                    
             </div>
         </>
     )
+    }else if(data.length > 15 && data.length <= 30){
+        const array10 = data.slice(0,15);
+        const array20 = data.slice(15,30);
+        return(
+        <>
+            <Alert variant="primary">
+                <div style={{textAlign: "left"}}>
+                    <h5 className="text-info"><FontAwesomeIcon icon={faHandPaper}/> &nbsp;Conocimientos</h5>
+                </div>
+            </Alert>
+                <Container>
+                    <Row>
+                        <Col>
+                            <ul>
+                            {
+                                array10.map((item)=><li key={item.idConocimiento}>{item.descripcion} (Nivel {item.nivel})</li>)
+                            }
+                            </ul>
+                        </Col>
+                        <Col>
+                            <ul>
+                            {
+                                array20.map((item)=><li key={item.idConocimiento}>{item.descripcion} (Nivel {item.nivel})</li>)
+                            }
+                            </ul>
+                        </Col>
+                    </Row>
+                </Container>
+        </>
+    )
+    }else if(data.length > 30){
+        const array10 = data.slice(0,20);
+        const array20 = data.slice(20,60);
+        return(
+        <>
+            <Alert variant="primary">
+                <div style={{textAlign: "left"}}>
+                    <h5 className="text-info"><FontAwesomeIcon icon={faHandPaper}/> &nbsp;Conocimientos</h5>
+                </div>
+            </Alert>
+                <Container>
+                    <Row>
+                        <Col>
+                            <ul>
+                            {
+                                array10.map((item)=><li key={item.idConocimiento}>{item.descripcion} (Nivel {item.nivel})</li>)
+                            }
+                            </ul>
+                        </Col>
+                        <Col>
+                            <ul>
+                            {
+                                array20.map((item)=><li key={item.idConocimiento}>{item.descripcion} (Nivel {item.nivel})</li>)
+                            }
+                            </ul>
+                        </Col>
+                    </Row>
+                </Container>
+        </>
+    )
+    }
 }
 export default KnowledgesPanelComponent
--- src/components/LeftPanelComponent.jsx
+++ src/components/LeftPanelComponent.jsx
@@ -19,12 +19,19 @@ return(
                 </p>
             </div>
             <hr style={{backgroundColor:"white"}}/>
-            <div style={{textAlign: "center"}}>
+            <div>
+                <h5>Datos personales</h5>
+                <p>Edad: {data.datosPersonales.edad} años</p>
+                <p>Lugar de residencia: {data.datosPersonales.lugarResidencia}, {data.datosPersonales.estadoResidencia}</p>
+                {data.datosPersonales.dispBackOffice ? <p>Disponiblidad para trabajar en oficina</p> : ""}
+                {data.datosPersonales.dispViajar ? <p>Disponiblidad para viajar</p> : ""}
+                {data.datosPersonales.dispMudarse ? <p>Disponiblidad para cambiar de residencia</p> : ""}
+            </div>
+            <hr style={{backgroundColor:"white"}}/>
+            <div>
                 <h5>Contacto</h5>
                 {data.telefonos.length > 0 ? <p>Cel: {data.telefonos[0]} </p> : <p></p>}
-
                 {data.correos.length > 0 ? <p>Email: {data.correos[0]} </p> : <p></p>}
-                <p>{data.datosPersonales.lugarResidencia}, {data.datosPersonales.estadoResidencia}</p>
             </div>
             <hr style={{backgroundColor:"white"}}/>
     </Col>
